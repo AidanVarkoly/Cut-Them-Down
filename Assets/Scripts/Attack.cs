691e905e0c9ebb20e15186935b68d74f4ce6e740
Aidan Varkoly <47114022+AidanVarkoly@users.noreply.github.com>
2020-03-18 18:22:26 -0400

    Game

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Transform attackPoint;
+    public LayerMask EnemyLayer;
+
+    public float attackRange = 1f;
+    public int attackDamage = 1;
+
+    public float attackRate = 2f;
+    private float nextAttackTime = 0.5f;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attacking();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
+        }
+    }
+
+    void Attacking()
+    {
+
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, EnemyLayer);
+
+       
+        foreach(Collider2D Enemy in hits)
+        {
+            Enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public enum EnemyStates
+    {
+       idleState,
+       MoveToTree,
+       MoveToOtso
+
+    }
+
+    void setCurrentState(EnemyStates state)
+    {
+        currentState = state;
+        LastStateChange = Time.time;
+    }
+
+    [Header("Health")]
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    [Header("Attack")]
+    public int AttackDamage = 1;
+    public float AttackRate = 2f;
+    public float NextAttackTime = 2f;
+
+    [SerializeField]
+    [Header("AI")]
+    GameObject[] Trees;
+    int index;
+    public GameObject CurrentTree;
+    public GameObject Otso;
+    public float Speed = 2;
+
+    private bool attacked = false;
+    private bool decision = false;
+    private EnemyStates currentState;
+    private float LastStateChange = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        FindTree();
+        setCurrentState(EnemyStates.idleState);
+    }
+
+    private void FixedUpdate()
+    {
+        switch(currentState)
+        {
+            case EnemyStates.idleState:
+                break;
+            case EnemyStates.MoveToTree:
+                MoveToTree();
+                break;
+            case EnemyStates.MoveToOtso:
+                MoveToOtso();
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if(currentHealth == maxHealth)
+        {
+            setCurrentState(EnemyStates.MoveToTree);
+        }
+        if(currentHealth < maxHealth && decision == false)
+        {
+            attacked = true;
+            setCurrentState(EnemyStates.idleState);
+        }
+
+        if(attacked)
+        {
+            GetFunction();
+            decision = true;
+            attacked = false;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void FindTree()
+    {
+        Trees = GameObject.FindGameObjectsWithTag("Tree");
+        index = Random.Range(0, Trees.Length);
+        CurrentTree = Trees[index];
+    }
+
+    GameObject FindClosestTree(GameObject[] Trees)
+    {
+        CurrentTree = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (GameObject tree in Trees)
+        {
+            Vector3 directionToTarget = tree.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                CurrentTree = tree;
+            }
+        }
+
+        return CurrentTree;
+    }
+
+    public void GetFunction()
+    {
+       int FuncToChoose = Random.Range(0, 2);
+
+       switch(FuncToChoose)
+        {
+            case 0:
+                setCurrentState(EnemyStates.MoveToTree);
+                break;
+            case 1:
+                setCurrentState(EnemyStates.MoveToOtso);
+                break;
+        }
+    }
+
+    public void MoveToTree()
+    { 
+        float Dist = Vector3.Distance(CurrentTree.transform.position, transform.position);
+        if (Dist > 1.5)
+        { 
+            transform.position = Vector3.MoveTowards(transform.position, CurrentTree.transform.position, Speed * Time.deltaTime);
+        }
+        if (Dist == 1.5)
+        {
+
+            Attack();
+
+        }
+    }
+
+    public void MoveToOtso()
+    {
+        float Dist2 = Vector3.Distance(Otso.transform.position, transform.position);
+        if (Dist2 > 1.5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Otso.transform.position, Speed * 2 * Time.deltaTime);
+        }
+        if (Dist2 == 1.5)
+        {
+            Attack();
+        }
+
+        if(Dist2 > 10)
+        {
+            FindClosestTree(Trees);
+            setCurrentState(EnemyStates.MoveToTree);
+        }
+    }
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
